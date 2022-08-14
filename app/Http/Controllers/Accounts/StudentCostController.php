d4a46743753c06f715c0992809a19b1ce074eb64
OmorFaruk00 <omorfaruk5020@gmail.com>
2022-08-14 19:52:31 +0600

    password reset and change

--- app/Http/Controllers/Accounts/StudentCostController.php
+++ app/Http/Controllers/Accounts/StudentCostController.php
@@ -38,7 +38,7 @@ class StudentCostController extends Controller
 
         $lilha = Fund::where('name', 'LILHA')->first();
         if ($request->payBy == 'LILHA') {
-            if (!$lilha || !$lilha->tatal_cash) {
+            if (!$lilha || !$lilha->total_cash) {
                 return response(['message' => 'LILHA has not enough money'], 422);
             }
             if ($lilha && $lilha->total_cash < $request->lilha_pay) {
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,15 +7,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use illuminate\Auth\AuthenticationException;
-use App\Models\Employee;
 use App\Models\Role;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Str;
+use App\Models\Employee;
+use App\Models\Password_reset;
+use Carbon\Carbon;
+use Illuminate\Mail\Mailer;
+use Illuminate\Support\Facades\Mail;
+
 
 class UserController extends Controller
 {
     function login(Request $request)
     {
-
-
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
@@ -33,11 +38,11 @@ class UserController extends Controller
             if ($user->role) {
                 $role = Role::where('name', $user->role)->first();
             }
-            $token = $user->createToken($request->email, $role?$role->permissions:[])->plainTextToken;            
+            $token = $user->createToken($request->email, $role ? $role->permissions : [])->plainTextToken;
             $response = [
                 'user' => $user,
                 'token' => $token,
-                
+
             ];
             return response($response, 201);
         } catch (AuthenticationException $e) {
@@ -50,8 +55,97 @@ class UserController extends Controller
     {
 
         if (auth()->check()) {
+            $request->user()->currentAccessToken()->delete();
             auth()->logout();
-            return 'logout';
+        }
+        return response([
+            "message" => 'Logout Successfull.'
+        ], 200);
+    }
+
+    public function Change_Password(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised(),
+            ],
+        ]);
+        try {
+            $id = auth()->user()->id;
+            $user = Employee::where('id', $id)->first();
+            if (!Hash::check($request->old_password, $user->password)) {
+                return response([
+                    "message" => 'The given password is invalid.', 'status' => 203
+                ]);
+            }
+            $user->password = Hash::make($request->password);
+            $user->save();
+            return response(['message' => 'Password Change Successfully', 'status' => 201]);
+        } catch (AuthenticationException $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
+    }
+
+
+    public function Password_Reset(Request $request, $email)
+    {
+        try {
+            $user = Employee::where('email', $email)->first();
+            if (!$user) {
+                return response(['message' => 'Email doesnt exists', 'status' => 404]);
+            }
+
+            $token = Str::random(50);
+            Password_reset::create([
+                'email' => $email,
+                'token' => $token,
+                'created_at' => Carbon::now()
+            ]);
+
+            Mail::send('reset', ['token' => $token], function ($message) use ($email) {
+                $message->subject('Password Reset');
+                $message->to($email);
+            });
+            return response(['message' => 'Password Reset Email Send... Check Your Email', 'status' => 200]);
+        } catch (AuthenticationException $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
+    }
+    public function Password_Reset_Confirm(Request $request, $token)
+    {
+        $request->validate([
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised(),
+            ],
+        ]);
+        try {
+            $PasswordReset = Password_reset::where('token', $token)->first();
+            if (!$PasswordReset) {
+                return response(['message' => 'Token is Invalid or Expired', 'status' => 404]);
+            }
+            $user = Employee::where('email', $PasswordReset->email)->first();
+            $user->password = Hash::make($request->password);
+            $user->save();
+            $PasswordReset = Password_reset::where('email', $user->email)->delete();
+
+            return response(['message' => 'Password Reset Successfully', 'status' => 201]);
+        } catch (AuthenticationException $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
         }
     }
 }
